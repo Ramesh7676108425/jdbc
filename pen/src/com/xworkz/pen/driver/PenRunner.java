d4668eb5b44fe1f8aabd056c98c7454496a3c96a
unknown <rameshl.xworkz@gmail.com>
2024-01-30 15:58:57 +0530

    jdbc

--- pen/src/com/xworkz/pen/driver/PenRunner.java
+++ pen/src/com/xworkz/pen/driver/PenRunner.java
@@ -7,26 +7,27 @@ import java.sql.Statement;
 import com.xworkz.pen.constants.Constantpen;
 
 public class PenRunner {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		try(Connection connection=DriverManager.getConnection(Constantpen.URL.getValue()
-				
+
+		try (Connection connection = DriverManager.getConnection(Constantpen.URL.getValue()
+
 				, Constantpen.USERNAME.getValue()
-				
+
 				, Constantpen.PASSWORD.getValue());
-				
-				Statement statement=connection.createStatement()){
+
+				Statement statement = connection.createStatement()) {
 			System.out.println("class is connected");
-			String query="INSERT INTO pen_details values('xyz','1')";
+			String query = "INSERT INTO pen_details  (name,cost)values('abcd',10),('luna quill',20),('orion ',30),('maverik quill',40),"
+					+ "('nebula nom de plume',50),('ballpoint',60),('gel pen',70),('highlighter',80),('marker pen',100),('red pen',120),('black pen',130),('blue pen',150)"
+					
+				;
 			statement.execute(query);
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			System.out.println("class is not connected ");
 			e.printStackTrace();
-			
+
 		}
 	}
 
